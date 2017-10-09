e7b1f01e66ace19d616b88d33c37f98a55b07ccd
begimov <begimov@gmail.com>
2017-10-09 06:42:30 +0300

    double ended queue implementation added

---
+++ Queues/DeQueue.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ *
+ */
+class DeQueue
+{
+    private $limit;
+    private $queue;
+
+    public function __construct($limit = 20)
+    {
+        $this->limit = $limit;
+        $this->queue = new LinkedList();
+    }
+
+    public function dequeueFromFront()
+    {
+        if ($this->isEmpty()) {
+            throw new UnderflowException('Queue is empty');
+        } else {
+            $lastItem = $this->peekFront();
+            $this->queue->deleteFirst();
+            return $lastItem;
+        }
+    }
+
+    public function dequeueFromBack()
+    {
+        if ($this->isEmpty()) {
+            throw new UnderflowException('Queue is empty');
+        } else {
+            $lastItem = $this->peekBack();
+            $this->queue->deleteLast();
+            return $lastItem;
+        }
+    }
+
+    public function enqueueAtBack(string $newItem)
+    {
+        if ($this->queue->getSize() < $this->limit) {
+            $this->queue->insert($newItem);
+        } else {
+            throw new OverflowException('Queue is full');
+        }
+    }
+
+    public function enqueueAtFront(string $newItem)
+    {
+        if ($this->queue->getSize() < $this->limit) {
+            $this->queue->insertAtFirst($newItem);
+        } else {
+            throw new OverflowException('Queue is full');
+        }
+    }
+
+    public function peekFront()
+    {
+        return $this->queue->getNthNode(1)->data;
+    }
+
+    public function peekBack()
+    {
+        return $this->queue->getNthNode($this->queue->getSize())->data;
+    }
+
+    public function isEmpty()
+    {
+        return $this->queue->getSize() == 0;
+    }
+}
